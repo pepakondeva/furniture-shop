08bed24a8694575906ca028dba1d0c08fc461a70
Pirinka Kondeva <167126915+pepakondeva@users.noreply.github.com>
2025-07-06 00:29:58 +0300

    Temporary: enable DEBUG to diagnose 500 error

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("DJANGO_SECRET_KEY")
-DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
+
+# DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
